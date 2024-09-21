a98c90c12974ea7e426f3e7f05e30755f9db6237
galyahikbariyeh <ghaliahjehad66@gmail.com>
2024-09-21 17:41:24 +0300

    lec11

---
+++ js/index.js
@@ -0,0 +1,50 @@
+console.log('hello')
+//data types js
+
+//global scope
+var age=25
+// age=30
+console.log('this is global var',age)
+
+//local scope
+let age1=25
+age1=43
+console.log('this is local var',age1)
+
+//constant
+const age2=25
+console.log('constant age',age2)
+
+ var test1=('this is test variable')
+console.log('test variable :',test1)
+// var x=2
+// var c=3
+// var z=c+x
+// console.log('add:',z)
+var isActive = true
+console.log("is active",isActive)
+
+//list and array
+var names =["ahmad","ali","mohammad"]
+console.log("names :",names)
+
+var sample= null
+console.log("sample :",sample)
+
+// if statement
+// ==: value ,=== : value and datatype
+// => =< != ==> ==<
+
+if(isActive==false){
+    console.log('this is true condition')
+}
+else{
+    console.log('this is false condition')
+}
+
+if(age2==='25'){
+    console.log('this is true condition')
+}
+else{
+    console.log('this is false condition')
+}
